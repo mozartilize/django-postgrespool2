39e122bfe2b3607569bba329e85cdf0841284b28
arshavindn <mozartilize@gmail.com>
2019-07-25 13:18:16 +0700

    use raw connection

--- django_postgrespool2/base.py
+++ django_postgrespool2/base.py
@@ -13,23 +13,23 @@ if 'Psycopg2DatabaseWrapper' not in globals():
         # Django >= 1.9
         from django.db.backends.postgresql.base import (
             psycopg2,
-            PSYCOPG2_VERSION,
             Database,
             DatabaseWrapper as Psycopg2DatabaseWrapper,
         )
         from django.db.backends.postgresql.creation import (
             DatabaseCreation as Psycopg2DatabaseCreation,
         )
+        from django.db.backends.postgresql.utils import utc_tzinfo_factory
     except ImportError:
         from django.db.backends.postgresql_psycopg2.base import (
             psycopg2,
             Database,
-            PSYCOPG2_VERSION,
             DatabaseWrapper as Psycopg2DatabaseWrapper,
         )
         from django.db.backends.postgresql_psycopg2.creation import (
             DatabaseCreation as Psycopg2DatabaseCreation,
         )
+        from django.db.backends.postgresql_psycopg2.utils import utc_tzinfo_factory
 
 
 # DATABASE_POOL_ARGS should be something like:
@@ -49,13 +49,6 @@ log = logging.getLogger('z.pool')
 
 def _log(message, *args):
     log.debug(message)
-
-
-@event.listens_for(pool_cls, 'connect')
-def receive_connect(dbapi_conn, conn_record):
-    # psycopg 2.8 add connection info thus assign connection record info to it
-    if PSYCOPG2_VERSION >= (2, 8, 0):
-        conn_record.info = dbapi_conn.info
 
 
 # Only hook up the listeners if we are in debug mode.
@@ -85,21 +78,28 @@ class DatabaseWrapper(Psycopg2DatabaseWrapper):
         super(DatabaseWrapper, self).__init__(*args, **kwargs)
         self._pool = pool_cls(
             lambda: get_conn(**self.get_connection_params()), **pool_args)
+        self._pool_connection = None
         self.creation = DatabaseCreation(self)
 
     @property
     def pool(self):
         return self._pool
 
-    def _commit(self):
-        if self.connection is not None and self.is_usable():
+    def _close(self):
+        if self._pool_connection is not None:
             with self.wrap_database_errors:
-                return self.connection.commit()
+                return self._pool_connection.close()
 
-    def _rollback(self):
-        if self.connection is not None and self.is_usable():
-            with self.wrap_database_errors:
-                return self.connection.rollback()
+    def create_cursor(self, name=None):
+        if name:
+            # In autocommit mode, the cursor will be used outside of a
+            # transaction, hence use a holdable cursor.
+            cursor = self._pool_connection.cursor(
+                name, scrollable=False, withhold=self.connection.autocommit)
+        else:
+            cursor = self._pool_connection.cursor()
+        cursor.tzinfo_factory = utc_tzinfo_factory if settings.USE_TZ else None
+        return cursor
 
     def dispose(self):
         """Dispose of the pool for this instance, closing all connections."""
@@ -108,24 +108,23 @@ class DatabaseWrapper(Psycopg2DatabaseWrapper):
 
     def get_new_connection(self, conn_params):
         # get new connection through pool, not creating a new one outside.
-        c = self.pool.connect()
+        self._pool_connection = self.pool.connect()
+        c = self._pool_connection.connection  # dbapi connection
 
         options = self.settings_dict['OPTIONS']
         try:
             self.isolation_level = options['isolation_level']
         except KeyError:
-            self.isolation_level = c.connection.isolation_level
+            self.isolation_level = c.isolation_level
         else:
             # Set the isolation level to the value from OPTIONS.
-            if self.isolation_level != c.connection.isolation_level:
-                c.connection.set_session(isolation_level=self.isolation_level)
+            if self.isolation_level != c.isolation_level:
+                c.set_session(isolation_level=self.isolation_level)
 
         return c
 
-    def _set_autocommit(self, autocommit):
-        with self.wrap_database_errors:
-            self.connection.connection.autocommit = autocommit
-
     def is_usable(self):
         # https://github.com/kennethreitz/django-postgrespool/issues/24
-        return self.connection.is_valid
+        if self._pool_connection is None:
+            return False
+        return self._pool_connection.is_valid
